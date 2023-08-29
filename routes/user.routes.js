061d60fb06f3b24aefc4a843aa19924a4612a3f0
nacho <nachosanson6@gmail.com>
2023-08-29 21:44:13 +0200

    version 2 detalles y edicion de usuario

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,5 +14,15 @@ router.get('/details/:user_id', (req, res, next) => {
 
 })
 
+router.put('/edit/:user_id', (req, res, next) => {
+    const { user_id } = req.params
+    const { username, email, avatar, instruments, description } = req.body
+    console.log('hasta aqui llegooooo', user_id)
+    User
+        .findByIdAndUpdate(user_id, { username, email, avatar, instruments, description })
+        .then(() => res.sendStatus(201))
+        .catch(err => next(err))
+})
+
 
 module.exports = router
